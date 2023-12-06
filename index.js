efe52d5395811a4108a06a2ad973c63d00ea0d1b
Jesús Guzmán Romero <61877965+Jguzmanr07@users.noreply.github.com>
2023-12-06 14:46:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 import { handleEvent } from 'flareact'
 import { processCronTrigger } from './src/functions/cronTrigger'
 
-/**
- * The DEBUG flag will do two things that help during development:
- * 1. we will skip caching on the edge, which makes it easier to
- *    debug.
- * 2. we will return an error message on exception in your Response rather
- *    than the default 404.html page.
- */
-const DEBUG = false
+const DEBUG = false;
 
 addEventListener('fetch', (event) => {
   try {
@@ -30,3 +23,41 @@ addEventListener('fetch', (event) => {
 addEventListener('scheduled', (event) => {
   event.waitUntil(processCronTrigger(event))
 })
+
+// Agregar cabeceras a todas las respuestas
+addEventListener('fetch', (event) => {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
+  try {
+    // Realizar la solicitud original al servidor
+    let response = await fetch(request);
+
+    // Verificar si la respuesta es un error
+    if (!response.ok) {
+      // Si es un error, simplemente retornar la respuesta original
+      return response;
+    }
+
+    // Clonar la respuesta para poder modificar las cabeceras
+    let newResponse = new Response(response.body, response);
+
+    // Agregar las nuevas cabeceras
+    newResponse.headers.set('Cache-Control', 'no-cache, no-store, must-revalidate');
+    newResponse.headers.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+    newResponse.headers.set('X-Xss-Protection', '0');
+    newResponse.headers.set('X-Frame-Options', 'DENY');
+    newResponse.headers.set('X-Content-Type-Options', 'nosniff');
+    newResponse.headers.set('X-Permitted-Cross-Domain-Policies', 'none');
+    newResponse.headers.set('Referrer-Policy', 'strict-origin');
+    newResponse.headers.set('Expires', '0');
+    newResponse.headers.set('Pragma', 'no-cache');
+    // Agregar cualquier otra cabecera necesaria aquí
+
+    return newResponse;
+  } catch (error) {
+    // Manejar errores y retornar una respuesta adecuada
+    return new Response('Error occurred: ' + error.message, { status: 500 });
+  }
+}
